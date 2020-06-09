f6ba37400434d462067ab961f98e80ae9b36afed
Sean Cross <sean@xobs.io>
2020-06-09 13:11:09 +0800

    main: run through clangfmt
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.c
+++ src/main.c
@@ -207,35 +207,35 @@ void demodulate_u16(int16_t *samples, size_t count) {
 void write_wav(int16_t *data, size_t len, const char *name) {
     int out_fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if (out_fd == -1) {
-      perror("unable to open filtered file");
-      return 1;
+        perror("unable to open filtered file");
+        return 1;
     }
     if (write(out_fd, wave_header, sizeof(wave_header)) == -1) {
         perror("error");
     }
-    if (write(out_fd, data, len) ==
-        -1) {
+    if (write(out_fd, data, len) == -1) {
         perror("error");
     }
     close(out_fd);
 }
 
-void write_wav_stereo(int16_t *left, int16_t *right, size_t len, const char *name) {
+void write_wav_stereo(int16_t *left, int16_t *right, size_t len,
+                      const char *name) {
     int out_fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if (out_fd == -1) {
-      perror("unable to open filtered file");
-      return 1;
+        perror("unable to open filtered file");
+        return 1;
     }
     if (write(out_fd, wave_header, sizeof(wave_header)) == -1) {
         perror("error");
     }
-    for(int i = 0; i < len; i++ ) {
-      if (write(out_fd, &(left[i]), 2) == -1) {
-        perror("error");
-      }
-      if (write(out_fd, &(right[i]), 2) == -1) {
-        perror("error");
-      }
+    for (int i = 0; i < len; i++) {
+        if (write(out_fd, &(left[i]), 2) == -1) {
+            perror("error");
+        }
+        if (write(out_fd, &(right[i]), 2) == -1) {
+            perror("error");
+        }
     }
     close(out_fd);
 }
@@ -302,24 +302,27 @@ void filter_u16(int16_t *src, int16_t *dest, size_t count) {
 }
 
 struct nco_state {
-  float32_t samplerate; // Hz
-  float32_t freq;       // Hz
-  
-  float32_t phase;      // rad
+    float32_t samplerate; // Hz
+    float32_t freq;       // Hz
+
+    float32_t phase; // rad
 } nco_st;
 
 void nco(float32_t control, uint32_t timestep, float32_t *i, float32_t *q) {
-  // control is a number from +1 to -1, which translates to -pi to +pi additional phase per time step
-  // timestep is the current time step, expressed in terms of samples since t=0
-  // i is a pointer to the in-phase return value
-  // q is a pointer to the quadrature return value
+    // control is a number from +1 to -1, which translates to -pi to +pi additional phase per time step
+    // timestep is the current time step, expressed in terms of samples since t=0
+    // i is a pointer to the in-phase return value
+    // q is a pointer to the quadrature return value
 
-  nco_st.phase += (control / PI);
-  
-  *i = cos( (((float32_t)timestep) * nco_st.freq * 2 * PI) / nco_st.samplerate + nco_st.phase);
-  *q = sin( (((float32_t)timestep) * nco_st.freq * 2 * PI) / nco_st.samplerate + nco_st.phase);
+    nco_st.phase += (control / PI);
+
+    *i =
+        cos((((float32_t)timestep) * nco_st.freq * 2 * PI) / nco_st.samplerate +
+            nco_st.phase);
+    *q =
+        sin((((float32_t)timestep) * nco_st.freq * 2 * PI) / nco_st.samplerate +
+            nco_st.phase);
 }
-
 
 int main(int argc, char **argv) {
     char *wave_file_name = "samples/PSK31_sample.wav";
@@ -367,9 +370,9 @@ int main(int argc, char **argv) {
     wave_filtered = malloc(baselen * sizeof(int16_t));
     memset(wave_filtered, 0, baselen * sizeof(int16_t));
 
-    int samplerate = *((unsigned int *) &(wave_header[24]));
+    int samplerate = *((unsigned int *)&(wave_header[24]));
     printf("samplerate: %d\n", samplerate);
-    
+
     // double the sampling rate through interpolation and filtering
     baselen = baselen * 2;
     wave_upsampled = malloc(baselen * sizeof(int16_t));
@@ -378,31 +381,33 @@ int main(int argc, char **argv) {
     memset(wave_upsampled, 0, baselen * sizeof(int16_t));
     memset(wave_upsampled_filtered, 0, baselen * sizeof(int16_t));
     samplerate = samplerate * 2;
-    *((unsigned int *) &(wave_header[24])) = samplerate;
+    *((unsigned int *)&(wave_header[24])) = samplerate;
 
-    for( int i = 0; i < baselen / 2; i ++ ) {
-      wave_upsampled[i*2] = wave_buffer[i];
-      wave_upsampled[i*2+1] = 0;
+    for (int i = 0; i < baselen / 2; i++) {
+        wave_upsampled[i * 2] = wave_buffer[i];
+        wave_upsampled[i * 2 + 1] = 0;
     }
-    
+
     arm_fir_instance_f32 fir;
     arm_fir_init_f32(&fir, FIR_STAGES, fir_coefficients, fir_state,
                      SAMPLES_PER_PERIOD);
     for (int sample_offset = 0; sample_offset < baselen;
          sample_offset += SAMPLES_PER_PERIOD) {
-      // Apply our bandpass filter
-      float32_t firwindow[SAMPLES_PER_PERIOD];
-      arm_q15_to_float(&(wave_upsampled[sample_offset]), firwindow, SAMPLES_PER_PERIOD);
-      static float32_t filtered_samples[SAMPLES_PER_PERIOD];
-      arm_fir_f32(&fir, firwindow, filtered_samples,
-		  SAMPLES_PER_PERIOD);
-      arm_float_to_q15(filtered_samples, &(wave_upsampled_filtered[sample_offset]), SAMPLES_PER_PERIOD);
+        // Apply our bandpass filter
+        float32_t firwindow[SAMPLES_PER_PERIOD];
+        arm_q15_to_float(&(wave_upsampled[sample_offset]), firwindow,
+                         SAMPLES_PER_PERIOD);
+        static float32_t filtered_samples[SAMPLES_PER_PERIOD];
+        arm_fir_f32(&fir, firwindow, filtered_samples, SAMPLES_PER_PERIOD);
+        arm_float_to_q15(filtered_samples,
+                         &(wave_upsampled_filtered[sample_offset]),
+                         SAMPLES_PER_PERIOD);
     }
-    
+
     write_wav(wave_upsampled_filtered, baselen, "upsampled_filtered.wav");
 
     // now try to run a PLL to "lock" onto the signal
-    nco_st.samplerate = (float32_t) samplerate;
+    nco_st.samplerate = (float32_t)samplerate;
     nco_st.freq = 1000.00;
     nco_st.phase = 0.0;
 
@@ -414,19 +419,21 @@ int main(int argc, char **argv) {
     memset(i_int, 0, baselen);
     memset(q_int, 0, baselen);
 
-    for( int i = 0; i < baselen; i++ ) {
-      float32_t i_flt;
-      float32_t q_flt;
+    for (int i = 0; i < baselen; i++) {
+        float32_t i_flt;
+        float32_t q_flt;
 
-      nco(0.0, (uint32_t) i, &i_flt, &q_flt);
-      i_int[i] = (int16_t) (i_flt * 8000);
-      q_int[i] = (int16_t) (q_flt * 8000);
+        nco(0.0, (uint32_t)i, &i_flt, &q_flt);
+        i_int[i] = (int16_t)(i_flt * 8000);
+        q_int[i] = (int16_t)(q_flt * 8000);
     }
     wave_header[22] = 2; // stereo
-    uint32_t byterate = *((uint32_t *) &(wave_header[28]));
-    *((uint32_t *) &(wave_header[28])) = byterate * 2;
-    uint32_t length = *((uint32_t *) &(wave_header[40]));
-    *((uint32_t *) &(wave_header[40])) = length * 4; // *4 because we forgot to expand it when we doubled sample rate
+    uint32_t byterate = *((uint32_t *)&(wave_header[28]));
+    *((uint32_t *)&(wave_header[28])) = byterate * 2;
+    uint32_t length = *((uint32_t *)&(wave_header[40]));
+    *((uint32_t *)&(wave_header[40])) =
+        length *
+        4; // *4 because we forgot to expand it when we doubled sample rate
     write_wav_stereo(i_int, q_int, baselen, "quadrature.wav");
     ///////
 
@@ -447,46 +454,49 @@ int main(int argc, char **argv) {
     float32_t nco_error = 0.0;
     for (int sample_offset = 0; sample_offset < baselen;
          sample_offset += SAMPLES_PER_PERIOD) {
-      
-      float32_t loopwindow[SAMPLES_PER_PERIOD];
-      arm_q15_to_float(&(wave_upsampled_filtered[sample_offset]), loopwindow, SAMPLES_PER_PERIOD);
 
-      float32_t i_samps[SAMPLES_PER_PERIOD];
-      float32_t q_samps[SAMPLES_PER_PERIOD];
-      for(int i = 0; i < SAMPLES_PER_PERIOD; i++) {
-	nco(nco_error, (uint32_t) (i + sample_offset), &(i_samps[i]), &(q_samps[i]));
-      }
-      //arm_q15_to_float(&(i_int[sample_offset]), i_samps, SAMPLES_PER_PERIOD);
-      //arm_q15_to_float(&(q_int[sample_offset]), q_samps, SAMPLES_PER_PERIOD);
-	
-      static float32_t i_mult_samps[SAMPLES_PER_PERIOD];
-      static float32_t q_mult_samps[SAMPLES_PER_PERIOD];
-      arm_mult_f32(loopwindow, i_samps, i_mult_samps, SAMPLES_PER_PERIOD);
-      arm_mult_f32(loopwindow, q_samps, q_mult_samps, SAMPLES_PER_PERIOD);
-      
-      static float32_t i_lpf_samples[SAMPLES_PER_PERIOD];
-      static float32_t q_lpf_samples[SAMPLES_PER_PERIOD];
-      arm_fir_f32(&i_lpf, i_mult_samps, i_lpf_samples, SAMPLES_PER_PERIOD);
-      arm_fir_f32(&q_lpf, q_mult_samps, q_lpf_samples, SAMPLES_PER_PERIOD);
-      
-      arm_float_to_q15(i_lpf_samples, &(i_loop[sample_offset]), SAMPLES_PER_PERIOD);
-      arm_float_to_q15(q_lpf_samples, &(q_loop[sample_offset]), SAMPLES_PER_PERIOD);
+        float32_t loopwindow[SAMPLES_PER_PERIOD];
+        arm_q15_to_float(&(wave_upsampled_filtered[sample_offset]), loopwindow,
+                         SAMPLES_PER_PERIOD);
 
-      float32_t errorwindow[SAMPLES_PER_PERIOD];
-      arm_mult_f32(i_lpf_samples, q_lpf_samples, errorwindow, SAMPLES_PER_PERIOD);
-      float32_t avg = 0;
-      for(int i = 0; i < SAMPLES_PER_PERIOD; i++) {
-	avg += errorwindow[i];
-      }
-      avg /= ((float32_t) SAMPLES_PER_PERIOD);
-      nco_error = -(avg);
-      // printf("err: %0.04f\n", nco_error);
+        float32_t i_samps[SAMPLES_PER_PERIOD];
+        float32_t q_samps[SAMPLES_PER_PERIOD];
+        for (int i = 0; i < SAMPLES_PER_PERIOD; i++) {
+            nco(nco_error, (uint32_t)(i + sample_offset), &(i_samps[i]),
+                &(q_samps[i]));
+        }
+        //arm_q15_to_float(&(i_int[sample_offset]), i_samps, SAMPLES_PER_PERIOD);
+        //arm_q15_to_float(&(q_int[sample_offset]), q_samps, SAMPLES_PER_PERIOD);
 
+        static float32_t i_mult_samps[SAMPLES_PER_PERIOD];
+        static float32_t q_mult_samps[SAMPLES_PER_PERIOD];
+        arm_mult_f32(loopwindow, i_samps, i_mult_samps, SAMPLES_PER_PERIOD);
+        arm_mult_f32(loopwindow, q_samps, q_mult_samps, SAMPLES_PER_PERIOD);
+
+        static float32_t i_lpf_samples[SAMPLES_PER_PERIOD];
+        static float32_t q_lpf_samples[SAMPLES_PER_PERIOD];
+        arm_fir_f32(&i_lpf, i_mult_samps, i_lpf_samples, SAMPLES_PER_PERIOD);
+        arm_fir_f32(&q_lpf, q_mult_samps, q_lpf_samples, SAMPLES_PER_PERIOD);
+
+        arm_float_to_q15(i_lpf_samples, &(i_loop[sample_offset]),
+                         SAMPLES_PER_PERIOD);
+        arm_float_to_q15(q_lpf_samples, &(q_loop[sample_offset]),
+                         SAMPLES_PER_PERIOD);
+
+        float32_t errorwindow[SAMPLES_PER_PERIOD];
+        arm_mult_f32(i_lpf_samples, q_lpf_samples, errorwindow,
+                     SAMPLES_PER_PERIOD);
+        float32_t avg = 0;
+        for (int i = 0; i < SAMPLES_PER_PERIOD; i++) {
+            avg += errorwindow[i];
+        }
+        avg /= ((float32_t)SAMPLES_PER_PERIOD);
+        nco_error = -(avg);
+        // printf("err: %0.04f\n", nco_error);
     }
     printf("\n");
     write_wav_stereo(i_loop, q_loop, baselen, "quadrature_loop.wav");
-    
-    
+
     /*
     filter_u16(wave_buffer, wave_filtered,
                (statbuf.st_size - sizeof(wave_header)) / sizeof(*wave_buffer));
