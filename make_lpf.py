1169802ca68fdd92ff0fb68bde6ec3b2cf0688e1
bunnie <bunnie@kosagi.com>
2020-06-10 01:23:44 +0800

    increase lpf filter width to 16, works a bit better

--- make_lpf.py
+++ make_lpf.py
@@ -52,7 +52,7 @@ def bandpass_remez(ntaps, lowcut, highcut, fs, width):
 taps = 16
 rate = 62500
 tone = 20840
-bandwidth = 20
+bandwidth = 100
 coefficients = create_filter(rate=rate, tone=tone, bandwidth=bandwidth, taps=taps, window="hamming")
 # coefficients = bandpass_remez(taps, (1000 - bandwidth / 2), (1000 + bandwidth / 2), rate, 1.0)
 # coefficients = bandpass_kaiser(taps, (1000 - bandwidth / 2), (1000 + bandwidth / 2), rate, 1.0)
